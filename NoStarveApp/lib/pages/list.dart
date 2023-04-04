16291bc4de286e5b5186967c9783f3514e7bd7e9
parthibdas <91630684+parthibdas@users.noreply.github.com>
2023-04-04 22:26:10 +0530

    Delete NoStarveApp directory

--- NoStarveApp/lib/pages/list.dart
+++
@@ -1,5 +0,0 @@
-List a = [
-  "https://thumbs.dreamstime.com/b/donating-food-to-poor-people-experiencing-lot-poverty-come-wait-free-154996178.jpg",
-  "https://as2.ftcdn.net/v2/jpg/02/78/16/03/1000_F_278160345_JCtJn4nHYm2FYxxl6LIiNNjyT7biOVLX.jpg",
-  "https://newsonair.com/wp-content/uploads/2023/01/food-security-696x522.jpg"
-];
--- NoStarveApp/lib/widgets/app_constants.dart
+++
@@ -1,8 +0,0 @@
-class AppConstants {
-  static const String APP_NAME = "NoStarve";
-  static const int APP_VERSION = 1;
-  static const String BASE_URL = "https://mvs.bslmeiyu.com";
-  static const POPULAR_PRODUCT_URI = "/api/v1/products/popular";
-  static const RECOMMENDED_PRODUCT_URI = "/api/v1/products/recommended";
-  static const String TOKEN = "NStoken";
-}
